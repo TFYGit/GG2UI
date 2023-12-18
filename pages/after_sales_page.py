d43f15040dadd15da8182806794c33c675d011f2
TFY <tfy125800@gmail.com>
2023-12-18 15:38:58 +0800

    售后页补充3个按钮定位

--- pages/after_sales_page.py
+++ pages/after_sales_page.py
@@ -10,4 +10,14 @@ class AfterSalesPage(Browser):
     chat_button = ('xpath', "(//div[@class='block cursor-pointer'])[1]")
 
     # 帮助中心按钮
+    help_center_button = ('xpath', "(//div[@class='block cursor-pointer'])[2]")
+
+    # 创建新工单按钮
+    work_order_button = ('xpath', "(//button[@type='button'])[2]")
+
+    # 搜索输入框
+    search_input = ('xpath', "//input[@placeholder='搜索订阅帐户...']")
+
+
+
 
